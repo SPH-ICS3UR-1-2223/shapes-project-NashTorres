1285fb6d87703a7e3f82138e9f4fd214dcc44b0c
NashTorres <113381172+NashTorres@users.noreply.github.com>
2022-11-03 14:21:27 -0400

    ShapesProject

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -9,52 +9,49 @@ public class Rectangle {
 	
 	//methods
 	public Rectangle() {
-		
-	}
-	
+		this.length = 1;
+		this.width = 1;
+	}	
 	public Rectangle(double side) {
-		
+		this.length = side;
+		this.width = side;
 	}
 	
 	public Rectangle(double length, double width) {
-		
+		this.length = length;
+		this.width = width;
+		this.area = findArea();
+		this.perimeter = findPerimeter();
 	}
-	
 	private double findPerimeter() {
-		return 0;
+		return this.length*2+this.width*2;
 	}
-	
 	private double findArea() {
-		return 0;
+		return this.length*this.width;
 	}
-	
 	public void printStats() {
-		
+		System.out.println("Rectangle");
+		System.out.println("Length = "+this.length);
+		System.out.println("Area = "+this.width);
+		System.out.println("Area = "+this.area);
+		System.out.println("Perimeter = "+this.perimeter);
 	}
-
 	public double getLength() {
 		return length;
 	}
-
 	public void setLength(double length) {
 		this.length = length;
 	}
-
 	public double getWidth() {
 		return width;
 	}
-
 	public void setWidth(double width) {
 		this.width = width;
 	}
-
 	public double getArea() {
 		return area;
 	}
-
 	public double getPerimeter() {
 		return perimeter;
-	}
-	
-	
+	}		
 }
--- src/shapes/Triangle.java
+++ src/shapes/Triangle.java
@@ -9,56 +9,53 @@ public class Triangle {
 	
 	//Methods
 	public Triangle() {
-		
+		this.sideA = 1;
+		this.sideB = 1;
+		this.sideC = 1;
 	}
-	
 	public Triangle(double a, double b, double c) {
-		
+		this.sideA = a;
+		this.sideB = b;
+		this.sideC = c;
+		this.perimeter = findPerimeter();
+		this.area = findArea();
 	}
-	
 	private double findPerimeter() {
-		return 0;
+		return this.sideA+this.sideB+this.sideC;
 	}
-	
 	private double findArea() {
-		return 0;
+		return (this.sideA * this.sideB)/2;
 	}
-	
 	public void printStats() {
-		
+	System.out.println("Triangle");
+	System.out.println("Side A = "+this.sideA);
+	System.out.println("Side B = "+this.sideB);
+	System.out.println("Side C = "+this.sideC);
+	System.out.println("Perimeter = "+this.perimeter);
+	System.out.println("Area = "+this.area);
 	}
-
 	public double getSideA() {
 		return sideA;
 	}
-
 	public void setSideA(double sideA) {
 		this.sideA = sideA;
 	}
-
 	public double getSideB() {
 		return sideB;
 	}
-
 	public void setSideB(double sideB) {
 		this.sideB = sideB;
 	}
-
 	public double getSideC() {
 		return sideC;
 	}
-
 	public void setSideC(double sideC) {
 		this.sideC = sideC;
 	}
-
 	public double getPerimeter() {
 		return perimeter;
 	}
-
 	public double getArea() {
 		return area;
-	}
-	
-	
+	}	
 }
